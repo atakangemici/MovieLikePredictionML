6e31128beb0073170b7fc154f4d923d29a326dea
Atakan Gemici <atakan.gemici@primeapps.io>
2022-05-25 17:29:02 +0300

    fixed.

--- MovieLikePrediction.py
+++ MovieLikePrediction.py
@@ -1,15 +1,17 @@
-import pandas
-from sklearn import linear_model
+#Çok Değişkenli Doğrusal Regresyon (Multiple Linear Regression) Projesi
 
-df = pandas.read_csv("movie_data.csv")
+import pandas #Pandas kütüphanesi ile veri işleme gerçekleştiricez. 
+from sklearn import linear_model #Sklearn modülünü regresyon işlemi için kullanacağız.
 
-x = df[['Views', 'CommentCount']]
-y = df['Likes']
+df = pandas.read_csv("movie_data.csv") #Pandas kütüphanesi ile data setini import ediyoruz.
 
-regr = linear_model.LinearRegression()
-regr.fit(x, y)
+x = df[['Views', 'CommentCount']] #İki değişkene dayalı bir değeri tahmini etme modeli için bağımsız iki değişken seçildi.
+y = df['Likes'] #Tahmin edeceğimiz bağımlı değişken seçildi.
 
-likes = regr.predict([[2564903, 22149]])
+regr = linear_model.LinearRegression() #LinearRegression() yöntemini doğrusal bir regresyon nesnesi oluşturmak için kullanıyoruz.
+regr.fit(x, y) #fit() bağımsız ve bağımlı değerleri parametre olarak alan ve regresyon nesnesini tanımlanan verilere atayan yöntemdir.
 
-print('Tahmini Beğenilme : ' , format(int(likes),',d'))
+likes = regr.predict([[2819118, 20573]]) #Filmin görüntelenme ve yorum sayısına dayalı olarak tahmin etmeye hazır bir regresyon modelimiz var.
+
+print('Tahmini Beğenilme : ' , format(int(likes),',d')) #Beğeni tahmini ekranda gösteriliyor.
 
